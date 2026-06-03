b54674b47cd8e2f9cbf9b883d0515c8abba64fe0
chcappetta <cjcappetta8@gmail.com>
2026-06-03 09:02:37 -0700

    Add registry for weather and calculator tools

---
+++ registry.py
@@ -0,0 +1,8 @@
+from tools.weather import weather_script
+from tools.calculator import quick_calc, full_calc
+
+TOOLS = {
+    "/weather": weather_script,
+    "/math": quick_calc,
+    "/calc": full_calc
+}
