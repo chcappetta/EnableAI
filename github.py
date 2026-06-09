36dc3251bddd2ddbb8d684724d2ae1d973557e32
chcappetta <cjcappetta8@gmail.com>
2026-06-08 18:31:49 -0700

    Add GitHub user info retrieval function

---
+++ github.py
@@ -0,0 +1,21 @@
+import requests
+
+HEADERS = {
+    "User-Agent": "EnableAI"
+}
+
+def github_tool(ai_name, username):
+    url = f"https://api.github.com/users/{username}"
+    response = requests.get(url, headers=HEADERS)
+    
+    if response.status_code != 200:
+        return(f"{ai_name}: Sorry, there was an error with your request. Try again.")
+    
+    data = response.json()
+    return (
+        f"GitHub User: {data['login']}\n"
+        f"Name: {data.get('name', 'N/A')}\n"
+        f"Public Repos: {data['public_repos']}\n"
+    )
+
+
