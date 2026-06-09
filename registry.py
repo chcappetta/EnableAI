61b93451d8ed56831b1a511bd413f6cb3c8d3741
chcappetta <cjcappetta8@gmail.com>
2026-06-08 18:32:14 -0700

    Add github_tool to TOOLS dictionary

--- registry.py
+++ registry.py
@@ -1,8 +1,10 @@
 from tools.weather import weather_script
 from tools.calculator import quick_calc, full_calc
+from tools.github import github_tool
 
 TOOLS = {
     "/weather": weather_script,
     "/math": quick_calc,
-    "/calc": full_calc
+    "/calc": full_calc,
+    "/github": github_tool
 }
