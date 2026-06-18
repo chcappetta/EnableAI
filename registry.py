a23da330dbe642dbbcdd2d1372f54589c1df9ff6
chcappetta <cjcappetta8@gmail.com>
2026-06-18 16:20:26 -0700

    Reorganize tools and add profile functionalities

--- registry.py
+++ registry.py
@@ -1,10 +1,13 @@
 from tools.weather import weather_script
 from tools.calculator import quick_calc, full_calc
 from tools.github import github_tool
+from tools.profile import remember_tool, profile_tool
 
 TOOLS = {
-    "/weather": weather_script,
-    "/math": quick_calc,
     "/calc": full_calc,
-    "/github": github_tool
+    "/github": github_tool,
+    "/math": quick_calc,
+    "/profile": profile_tool,
+    "/remember": remember_tool,
+    "/weather": weather_script
 }
